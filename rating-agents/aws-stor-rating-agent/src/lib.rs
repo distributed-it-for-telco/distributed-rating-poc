bc1c00c752e040ab51075f597d2cc6a71dd72025
Reda ElSayed <reda.el-sayed@orange.com>
2023-08-14 16:23:59 +0300

    implement was-stor agent(validation and rating)

--- rating-agents/aws-stor-rating-agent/src/lib.rs
+++ rating-agents/aws-stor-rating-agent/src/lib.rs
@@ -1,15 +1,17 @@
 use rating_interface::{
     AgentIdentifiation, RatingAgent, RatingAgentReceiver, RatingRequest, RatingResponse,
     RatingResponseBuilder, UsageCollector, UsageCollectorSender, UsageProofHandler,
-    UsageProofRequest, ValidationRequest, ValidationResponse,
+    UsageProofRequest, ValidationRequest, ValidationResponse, UsageCharacteristic,
 };
 use wasmbus_rpc::actor::prelude::*;
+use wasmcloud_interface_logging::info;
+use wasmcloud_interface_numbergen::generate_guid;
 
 const OFFER_ID: &str = "100";
 const DROPBOX_PARTY_ID_AT_PARTNER_SIDE: &str = "dropbox_my_partner";
 const PROVIDER_AGENT_NAME: &str = "aws_stor";
 const REPLICATION_FACTOR: u32 = 2;
-const RATE_FEE: f64 = ;
+const RATE_FEE: f64 = 1;
 
 #[derive(Debug, Default, Actor, HealthResponder)]
 #[services(Actor, RatingAgent)]
@@ -18,10 +20,58 @@ struct AwsStorRatingAgentActor {}
 #[async_trait]
 impl RatingAgent for AwsStorRatingAgentActor {
     async fn rate_usage(&self, _ctx: &Context, _arg: &RatingRequest) -> RpcResult<RatingResponse> {
-        todo!()
+        if _arg.usage.usage_characteristic_list.is_empty() {
+            return RpcResult::from(Err(RpcError::Other(
+                "Can't rate usage, no characteristic sent!".to_owned(),
+            )));
+        }
+
+        info!("Hello I'm your AWS stor postpaid rating agent");
+
+        let usage_date = "07/08/2023";
+        let usage_id: String = generate_guid().await?;
+
+        /*
+         *  Contract or Offer is 1 GB = 1 EUR
+         */
+
+        let mut storage_usage: f32 = 1;
+        for characteristic in _arg.usage.usage_characteristic_list.iter_mut() {
+            storage_usage *= characteristic.value.parse::<i32>().unwrap();
+        }
+
+        let rating = RATE_FEE * storage_usage;
+
+        let usage_template_str = UsageProofHandler::generate_rating_proof(&UsageProofRequest {
+            party_id: _arg.customer_id.to_owned(),
+            rating: rating.to_string(),
+            usage_characteristic_list: _arg.usage.usage_characteristic_list.to_owned(),
+            usage_id: usage_id.as_str().to_owned(),
+            usage_date: usage_date.to_owned(),
+            offer_id: OFFER_ID.to_owned(),
+        });
+
+        info!(
+            "Sending usage proof to usage collector for party with id: {}",
+            _arg.customer_id.to_string()
+        );
+
+        UsageCollectorSender::to_actor(&format!("mock/{}", "usage_collector_dropbox"))
+            .store(_ctx, &usage_template_str)
+            .await?;
+
+        let mut rating_response_builder = RatingResponseBuilder::new();
+
+        let rating_response = rating_response_builder
+            .unit((&"EUR").to_string())
+            .price(rating.to_string())
+            .authorized()
+            .build();
+
+        RpcResult::Ok(rating_response)
     }
 
-    /// Validate 
+    /// Validate
     /// 1- apply business validation.
     /// 2- translate usage.
     /// 3- return validation status and tranlated usage.
@@ -38,7 +88,6 @@ impl RatingAgent for AwsStorRatingAgentActor {
             validation_response.valid = false;
         }
 
-
         let mut next_agent: AgentIdentifiation = AgentIdentifiation::default();
 
         next_agent.name = PROVIDER_AGENT_NAME.to_string();
@@ -46,9 +95,23 @@ impl RatingAgent for AwsStorRatingAgentActor {
 
         validation_response.next_agent = Some(next_agent);
 
-        validation_response.translated_usage = Some(arg.rating_request.usage.to_owned());
+        let mut connectivity: f32 = 1;
+        for characteristic in arg.usage.usage_characteristic_list.iter_mut() {
+            connectivity *= characteristic.value.parse::<i32>().unwrap();
+        }
+
+        let mut translated_usage = arg.rating_request.usage.to_owned();
+        let connectivity_usage = UsageCharacteristic {
+            name: "connectivity".to_string(),
+            value: connectivity.to_string(),
+            value_type: "float".to_string(),
+        };
+        let mut characteristicsVector = vec![connectivity_usage];
+
+        translated_usage.usage_characteristic_list = characteristicsVector;
+
+        validation_response.translated_usage = Some(translated_usage);
         Ok(validation_response)
     }
+
 }
-
-
