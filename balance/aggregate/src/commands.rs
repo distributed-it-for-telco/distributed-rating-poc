ef9b4e0aa9cb909c5fcae6d983f2f5262669db6c
Mostafa Saied <mostafa.abdelwahab.ext@orange.com>
2024-02-20 20:34:58 +0200

    feat: impl balance aggregate

---
+++ balance/aggregate/src/commands.rs
@@ -0,0 +1,67 @@
+use crate::*;
+
+pub(crate) fn handle_create_balance(input: CreateBalance) -> Result<EventList> {
+    Ok(vec![Event::new(
+        BalanceCreated::TYPE,
+        STREAM,
+        &BalanceCreated {
+            balance: input.balance,
+            party_id: input.party_id,
+        },
+    )])
+}
+
+pub(crate) fn handle_deduct_balance(
+    input: DeductBalance,
+    state: Option<BalanceAggregateState>,
+) -> Result<EventList> {
+    let Some(state) = state else {
+        return Err(anyhow::anyhow!(
+            "Rejected command to deduct balance for party {}.",
+            input.party_id
+        ));
+    };
+
+    if state.available_balance() < input.amount as u32 {
+        error!(
+                "Rejecting command to deduct balance, party {} balance does not have sufficient amount. Available {}",
+                &input.party_id, state.available_balance()
+            );
+        Ok(vec![])
+    } else {
+        Ok(vec![Event::new(
+            BalanceDeducted::TYPE,
+            STREAM,
+            &BalanceDeducted {
+                amount: input.amount,
+                party_id: input.party_id,
+            },
+        )])
+    }
+}
+
+pub(crate) fn handle_deposit_balance(
+    input: DepositBalance,
+    state: Option<BalanceAggregateState>,
+) -> Result<EventList> {
+    if state.is_none() {
+        return Err(anyhow::anyhow!(
+            "Rejected command to deposit balance. party {} does not exist.",
+            input.party_id
+        ));
+    };
+
+    if input.amount as u32 < 0 {
+        error!("Rejecting command to deposit balance with negative amount");
+        Ok(vec![])
+    } else {
+        Ok(vec![Event::new(
+            BalanceDeposited::TYPE,
+            STREAM,
+            &FundsDeposited {
+                amount: input.amount,
+                party_id: input.party_id,
+            },
+        )])
+    }
+}
---
+++ balance/aggregate/src/lib.rs
@@ -0,0 +1,71 @@
+use anyhow::Result;
+use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
+use wasmcloud_interface_logging::error;
+
+mod commands;
+mod events;
+mod state;
+
+use state::BalanceAggregateState;
+
+concordance_gen::generate!({
+    path: "../eventcatalog",
+    role: "aggregate",
+    entity: "balance"
+});
+
+impl BalanceAggregate for BalanceAggregateImpl {
+    // -- Commands --
+    fn handle_create_balance(
+        &self,
+        input: CreateBalance,
+        _state: Option<BalanceAggregateState>,
+    ) -> anyhow::Result<EventList> {
+        commands::handle_create_balance(input)
+    }
+
+    fn handle_deduct_balance(
+        &self,
+        input: DeductBalance,
+        state: Option<BalanceAggregateState>,
+    ) -> anyhow::Result<EventList> {
+        commands::handle_deduct_balance(input, state)
+    }
+
+    fn handle_deposit_balance(
+        &self,
+        input: DepositBalance,
+        state: Option<BalanceAggregateState>,
+    ) -> anyhow::Result<EventList> {
+        commands::handle_deposit_balance(input, state)
+    }
+
+    // -- Events --
+    fn apply_balance_created(
+        &self,
+        input: BalanceCreated,
+        _state: Option<BalanceAggregateState>,
+    ) -> anyhow::Result<StateAck> {
+        events::apply_balance_created(input)
+    }
+
+    fn apply_balance_deposited(
+        &self,
+        input: BalanceDeposited,
+        state: Option<BankAccountAggregateState>,
+    ) -> anyhow::Result<StateAck> {
+        events::apply_balance_deposited(input, state)
+    }
+
+    fn apply_balance_deducted(
+        &self,
+        input: BalanceDeducted,
+        state: Option<BalanceAggregateState>,
+    ) -> anyhow::Result<StateAck> {
+        events::apply_balance_deducted(input, state)
+    }
+}
+
+const STREAM: &str = "balance";
