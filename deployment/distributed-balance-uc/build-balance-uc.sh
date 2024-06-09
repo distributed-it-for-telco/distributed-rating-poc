0a7c788c44ccc3bb059905ab75f810aad0c7f41c
Mostafa Saied <mostafa.abdelwahab.ext@orange.com>
2024-06-09 12:39:26 +0300

    feat: move concordance balance uder balance-management

--- deployment/distributed-balance-uc/build-balance-uc.sh
+++ deployment/distributed-balance-uc/build-balance-uc.sh
@@ -4,6 +4,6 @@ set -e
 cosmo build -p ../../api-gateway
 cosmo build -p ../../rating-coordinator
 cosmo build -p ../../rating-agents/prepaid-orange-vod-oneshot-cqrs-agent
-cosmo build -p ../../balance/eventcatalog/actor
-cosmo build -p ../../balance/projector
-cosmo build -p ../../balance/aggregate
+cosmo build -p ../../balance-management/event-sourced-balance/eventcatalog/actor
+cosmo build -p ../../balance-management/event-sourced-balance/projector
+cosmo build -p ../../balance-management/event-sourced-balance/aggregate
