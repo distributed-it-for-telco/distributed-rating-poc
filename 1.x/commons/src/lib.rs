148b15bab893807f0a9b0ea4b8900414b4337aa4
Ahmed Mostafa Rizk <ahmed.rizk.ext@orange.com>
2024-11-19 22:01:00 +0200

    add impl to generate_rating_proof

--- 1.x/commons/src/lib.rs
+++ 1.x/commons/src/lib.rs
@@ -4,6 +4,7 @@ wit_bindgen::generate!({
 
 use crate::exports::orange::commons::commons::*;
 use crate::exports::orange::commons::commons::Guest;
+use serde_json::json;
 
 struct Commons;
 
@@ -15,8 +16,35 @@ impl Guest for Commons {
     fn create_balance(count: f32, unit: String, party_id: String) -> Balance {
         Balance { count, unit, party_id }
     }
-    fn generate_rating_proof(request: UsageProofRequest) -> String{
-            "".to_string()
+    fn generate_rating_proof(usage_proof_request: UsageProofRequest) -> String{
+        let rating_date = "04/04/2023";
+
+        let usage_template_str = json!({
+            "id": usage_proof_request.usage_id,
+            "usageDate": usage_proof_request.usage_date,
+            "description": usage_proof_request.description,
+            "usageType": usage_proof_request.usage_type,
+            "ratedProductUsage": {
+                "isBilled": false,
+                "ratingAmountType": "Total",
+                "ratingDate": rating_date,
+                "bucketValueConvertedInAmount": {
+                    "unit": "EUR",
+                    "value": usage_proof_request.rating
+                },
+                "productRef": {
+                    "id": usage_proof_request.offer_id,
+                    "name": usage_proof_request.product_name
+                }
+            },
+            "relatedParty": {
+                "id": usage_proof_request.party_id
+            },
+            "usageCharacteristic": usage_proof_request.usage_characteristic_list
+        });
+
+        usage_template_str.to_string()
+
     }
 }
 
