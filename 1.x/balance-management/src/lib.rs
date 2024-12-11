dc25153272bb30a19dcf8330f74b1a22e73ad182
Ahmed Mostafa Rizk <ahmed.rizk.ext@orange.com>
2024-12-11 14:49:15 +0200

    refactor balance manager

--- 1.x/balance-management/src/lib.rs
+++ 1.x/balance-management/src/lib.rs
@@ -1,8 +1,10 @@
-use crate::orange::commons::types::{Balance};
+use crate::orange::commons::types::Balance;
+use crate::orange::commons::error_types::{GenericError, OtherError};
 use crate::orange::commons::mappers;
 
 use crate::exports::orange::balancemanager::balancemanager::*;
 use wasi::logging::logging::{log,Level::Info};
+use wasi::keyvalue::store;
 
 wit_bindgen::generate!({
     generate_all
@@ -15,7 +17,7 @@ struct BalanceManager;
 impl Guest for BalanceManager {
     fn get_balance(customer_id: String, offer_id: String) -> Balance {
        
-        let bucket = wasi::keyvalue::store::open("").expect("failed to open empty bucket");
+        let bucket = store::open("").expect("failed to open empty bucket");
         let object_name = format!("{}:{}:{}", "balance", customer_id,offer_id);
         log(Info, "", &object_name);
         
@@ -32,17 +34,17 @@ impl Guest for BalanceManager {
     }
 
     // Function to purchase and decrement the balance
-    fn purchase(mut balance: Balance, amount: f32, customer_id: String, offer_id: String) -> Result<Balance, String> {
+    fn purchase(mut balance: Balance, amount: f32, customer_id: String, offer_id: String) -> Result<Balance, GenericError> {
         if Self::has_sufficient_balance(balance.clone() ,amount) {
             balance.count -= amount;
 
-            let bucket = wasi::keyvalue::store::open("").expect("failed to open empty bucket");
+            let bucket = store::open("").expect("failed to open empty bucket");
             let object_name = format!("{}:{}:{}", "balance", customer_id,offer_id);
             let _ =  bucket.set(&object_name, &mappers::balance_to_stringified_array(&balance));
 
             Ok(Balance { count: balance.count, unit: balance.unit.clone(), party_id: balance.party_id.clone() })
         } else {
-            Err(String::from("Insufficient balance to purchase"))
+            Err(GenericError::Other(OtherError{message: "Insufficient balance to purchase".to_string()}))
         }
     }
 
